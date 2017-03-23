d1d30012644ada0d1d4937a2904988a9f7ecca88
gonzaloJC <gonzalo.garay@appdirect.com>
2017-03-23 13:46:38 -0300

    Adding testing.

--- src/Exercise1/test/ServiceTest.java
+++ src/Exercise1/test/ServiceTest.java
@@ -1,15 +1,33 @@
 package Exercise1.test;
 
-//import org.junit.Test;
-//
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import Exercise1.model.Device;
+import Exercise1.model.Status;
+import Exercise1.service.Service;
+
 
 /**
  * Created by computer on 22/03/17.
  */
 public class ServiceTest {
 
-//    @Test
-//    public void test(){
-//
-//    }
+    private Service service;
+
+
+    @Test
+    public void testGetServicseByDefault() {
+
+        service = new Service();
+        List<Device> listServiceActual = service.getDeviceByStatus(Status.CONNECTED);
+
+        for (Device dev : listServiceActual) {
+            assertTrue(Status.CONNECTED == dev.getStatus());
+        }
+
+    }
 }
